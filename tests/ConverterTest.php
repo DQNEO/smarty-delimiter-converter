2928cf64942df9e58f43bbf447e32843e21dd284
DQNEO <dqneoo@gmail.com>
2016-02-10 14:22:02 +0900

    add more tests

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -14,4 +14,26 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($converter instanceof Converter);
     }
 
+    public function test1()
+    {
+        $converter = new Converter(['{', '}'],['{', '}']);
+        $text = 'hello {$name}';
+        $this->assertSame($text, $converter->convert($text));
+        //$text = 'hello {$name}' . "\nworld";
+        //$this->assertSame($text, $converter->convert($text));
+    }
+
+    public function test2()
+    {
+        $converter = new Converter(['{', '}'],['{{', '}}']);
+        $text = 'hello {$name}';
+        $this->assertSame('hello {{$name}}', $converter->convert($text));
+    }
+
+    public function test3()
+    {
+        $converter = new Converter(['{', '}'],['<%', '%>']);
+        $text = 'hello { $name }';
+        $this->assertSame('hello <% $name %>', $converter->convert($text));
+    }
 }
