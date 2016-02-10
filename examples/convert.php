f19a5a4303dcf6c303e052840e7fbb0f8d9da85f
DQNEO <dqneoo@gmail.com>
2016-02-11 00:56:35 +0900

    gc

--- examples/convert.php
+++ examples/convert.php
@@ -9,18 +9,3 @@ $to = ['{','}'];
 $converter = new Converter($from, $to);
 
 echo $converter->convert($file);
-
-//echo $converter->convert($file);
-
-//$ret = $converter->convert(file_get_contents($file));
-//echo $ret;
-
-
-/*
-$smarty = new Smarty();
-$smarty->compile_dir = '.';
-$rendered = $smarty->fetch($file, null, null, false);
-echo $rendered;
-*/
-
-
